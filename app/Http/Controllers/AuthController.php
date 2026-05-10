446951645b9f7aed93c76464cdb3d8c6c24bfc81
EternalMpak <dapkness@gmail.com>
2026-05-10 13:54:59 +0300

    Fixed route in AuthController.php #2

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -36,7 +36,7 @@ class AuthController extends Controller
 
         Auth::login($user);
 
-        return redirect()->route('get_stats');
+        return redirect()->route('getStats');
     }
 
     public function showLoginForm(): Factory|View

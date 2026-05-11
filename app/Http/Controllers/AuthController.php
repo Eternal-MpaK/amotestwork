a9e31f592ddbf32c21a279940a20c22891bd210d
EternalMpak <dapkness@gmail.com>
2026-05-11 11:36:35 +0300

    Fixed AuthController.php - route redirection for login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -53,7 +53,7 @@ class AuthController extends Controller
 
         if (Auth::attempt($credentials, $request->boolean('remember'))) {
             $request->session()->regenerate();
-            return redirect()->intended(route('dashboard'));
+            return redirect()->intended(route('getStats'));
         }
 
         return back()->withErrors([
